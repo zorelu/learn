528a57c3309ec7d5daade83171d6b0f060132a50
zorelu <2703353611@qq.com>
2018-05-06 16:50:32 +0800

    Session

---
+++ study/flask/Session/Session.py
@@ -0,0 +1,63 @@
+from flask import Flask, session, redirect, url_for, escape, request
+import os
+from datetime import timedelta
+app = Flask(__name__)
+
+aa=os.urandom(24)
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        ##session超时设置
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=1)
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = aa
+
+
+app.run(host='0.0.0.0')
+
+
+###timeout set up
+# 构造函数：datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+# 其中参数都是可选，默认值为0
+#
+# 下面应该是常识，几乎每个人都知道：
+#
+#
+# 1 millisecond = 1000 microseconds
+# 1 minute = 60 seconds
+# 1 hour = 3600 seconds
+#
+# 1 week = 7 days
+# from datetime import date, timedelta
+#
+# print(timedelta.max);  # 999999999 days, 23:59:59.999999
+# print(timedelta(days=999999999, hours=23, minutes=59, seconds=59,
+#                 microseconds=999999));  # 999999999 days, 23:59:59.999999
+#
+# print(timedelta.min);  # -999999999 days, 0:00:00
+# print(timedelta(-999999999));  # -999999999 days, 0:00:00
+#
+# print(timedelta.resolution);  # 0:00:00.000001
+# print(timedelta(microseconds=1));  # 0:00:00.000001
---
+++ study/mysql.py
@@ -0,0 +1,21 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='tx.zorelu.win',
+                             user='root',
+                             password='123123zz',
+                             db='test',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+
+cursor = connection.cursor()
+        # Read a single record
+name=input()
+sql = "CREATE USER '{0}'@'localhost' IDENTIFIED BY '123456'".format(name)
+cursor.execute(sql)
+result = cursor.fetchone()
+print(result)
+
+connection.close()
