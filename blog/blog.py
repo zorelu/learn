6e5eefb7263cb7a4331e6859134cfc3de88683ce
zorelu <2703353611@qq.com>
2018-10-24 08:45:15 +0800

    blog day 2.5

--- blog/blog.py
+++ blog/blog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 import config
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config.from_object(config)
 def index():
     # remove the username from the session if it's there
 
-    return 'test'
+    return render_template('index.html')
 
 
 app.run(host='127.0.0.1')
