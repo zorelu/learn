f8e98dbc95cd37f6ebe541d557294849b0c7c11f
zorelu <2703353611@qq.com>
2018-11-01 22:52:57 +0800

    blog index pass

--- blog/blog.py
+++ blog/blog.py
@@ -14,9 +14,40 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    # remove the username from the session if it's there
+    # remove the username from the session if it's
+    ###按时间排序 -号
+    context = {
+        'questions' : Question.query.order_by('-create_time').all()
+    }
+    return render_template('index.html',**context)
 
-    return render_template('index.html')
+@app.route('/regist/', methods=['GET', 'POST'])
+def regist():
+    # remove the username from the session if it's there
+    if request.method == 'GET':
+        return render_template('regist.html')
+    else:
+        telephone = request.form['telephone']
+        username = request.form['username']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        user = User.query.filter(User.telephone == telephone).first()
+        # print (user)
+        if user:
+                return '手机号码已经被注册'
+        else:
+                if password1 != password2:
+                    return '密码重复'
+                else:
+
+                    user = User(telephone = telephone,username=username,password=password1)
+                    db.session.add(user)
+
+                    ###排查排查插入问题 注册插入数据报错(id must Intgeter)
+                    db.session.commit()
+                    return redirect(url_for('login'))
+
 
 @app.route('/login/',methods=['GET','POST'])
 def login():
@@ -42,32 +73,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/regist/', methods=['GET', 'POST'])
-def regist():
-    # remove the username from the session if it's there
-    if request.method == 'GET':
-        return render_template('regist.html')
-    else:
-        telephone = request.form['telephone']
-        username = request.form['username']
-        password1 = request.form['password1']
-        password2 = request.form['password2']
-
-        user = User.query.filter(User.telephone == telephone).first()
-        # print (user)
-        if user:
-                return '手机号码已经被注册'
-        else:
-                if password1 != password2:
-                    return '密码重复'
-                else:
-
-                    user = User(telephone = telephone,username=username,password=password1)
-                    db.session.add(user)
-
-                    ###排查排查插入问题 注册插入数据报错
-                    db.session.commit()
-                    return redirect(url_for('login'))
 
 @app.route('/question/', methods=['GET', 'POST'])
 @login_req
